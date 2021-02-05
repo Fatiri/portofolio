6d0dfd75d96507405c7240b4587477bc7d9b043b
fatiri <ilhamfatiriblog@gmail.com>
2021-02-05 08:20:48 +0700

    register childe store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex"
+import skill from "./module/skill";
+import serviceClient from "./module/service_client";
 
-Vue.use(Vuex)
 
+Vue.use(Vuex);
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    skill,
+    serviceClient
   }
 })
