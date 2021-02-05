dd87f86eeb4a131d2c63730d2e42d6f08a1fcb80
fatiri <ilhamfatiriblog@gmail.com>
2021-02-05 08:21:11 +0700

    fix layout

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -1,70 +1,89 @@
 <template>
-  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+  <div
+    class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 shadow-md"
+    :class="{ 'pb-1': isMobileDevice(), 'pb-10': isDesktopDevice() }"
+  >
     <div class="grid grid-rows-1 grid-flow-col gap-6">
       <div
-        class="-mx-3 md:flex mb-6 items-center mt-16 justify-center lg:ml-24 md:ml-24"
+        class="-mx-3 md:flex items-center mt-16 justify-center"
+        :class="{ 'mb-5 ': isMobileDevice(), 'mb-16 ml-24': isDesktopDevice() }"
       >
-        <div class="md:w-1/1 px-3 mb-6 md:mb-0">
-          <div class="profile"></div>
-          <img
-            class="profile-image bounce"
-            src="../assets/svg/me.png"
-            alt="Ilham Fatiri"
-          />
-          <div class="line-profile"></div>
-          <div class="-mx-3 flex mb-6 mt-1 justify-center">
+        <div
+          class="md:w-1/1 px-3 mb-6 md:mb-0"
+          :class="{ 'shadow-lg pb-10': isMobileDevice() }"
+        >
+          <div :class="{ 'px-6 ': isMobileDevice() }">
+            <div class="profile"></div>
+            <img
+              class="profile-image bounce"
+              src="../assets/svg/me.png"
+              alt="Ilham Fatiri"
+            />
+            <div class="line-profile"></div>
             <div
-              class="md:w-1/1 transform transition duration-500 hover:scale-125"
+              class="-mx-3 flex mt-1 justify-center"
+              :class="{ 'mb-0': isMobileDevice(), 'mb-6': isDesktopDevice() }"
             >
-              <a href="https://github.com/Fatiri" target="_blank">
-                <icon-svg class="mt-2" name="github" width="34" height="34" />
-              </a>
-            </div>
-            <div
-              class="md:w-1/1 px-3 custome-download-icon-sosmed transform transition duration-500 hover:scale-125"
-            >
-              <a href="https://www.linkedin.com/in/fatiri" target="_blank">
-                <icon-svg
-                  class="mt-2 ml-10"
-                  name="linkedin"
-                  width="34"
-                  height="34"
-                />
-              </a>
-            </div>
-            <div
-              class="md:w-1/1 px-3 custome-download-icon-sosmed transform transition duration-500 hover:scale-125"
-            >
-              <a href="https://www.instagram.com/ifatiri" target="_blank">
-                <icon-svg
-                  class="mt-2 ml-10"
-                  name="instagram"
-                  width="34"
-                  height="34"
-                />
-              </a>
-            </div>
-            <div
-              class="md:w-1/1 px-3 custome-download-icon-sosmed transform transition duration-500 hover:scale-125"
-            >
-              <a href="https://wa.link/e1raiy" target="_blank">
-                <icon-svg
-                  class="mt-2 ml-10"
-                  name="whatsup"
-                  width="34"
-                  height="34"
-                />
-              </a>
+              <div
+                class="md:w-1/1 transform transition duration-500 hover:scale-125"
+              >
+                <a href="https://github.com/Fatiri" target="_blank">
+                  <icon-svg class="mt-2" name="github" width="34" height="34" />
+                </a>
+              </div>
+              <div
+                class="md:w-1/1 px-3 custome-download-icon-sosmed transform transition duration-500 hover:scale-125"
+              >
+                <a href="https://www.linkedin.com/in/fatiri" target="_blank">
+                  <icon-svg
+                    class="mt-2 ml-10"
+                    name="linkedin"
+                    width="34"
+                    height="34"
+                  />
+                </a>
+              </div>
+              <div
+                class="md:w-1/1 px-3 custome-download-icon-sosmed transform transition duration-500 hover:scale-125"
+              >
+                <a href="https://www.instagram.com/ifatiri" target="_blank">
+                  <icon-svg
+                    class="mt-2 ml-10"
+                    name="instagram"
+                    width="34"
+                    height="34"
+                  />
+                </a>
+              </div>
+              <div
+                class="md:w-1/1 px-3 custome-download-icon-sosmed transform transition duration-500 hover:scale-125"
+              >
+                <a href="https://wa.link/e1raiy" target="_blank">
+                  <icon-svg
+                    class="mt-2 ml-10"
+                    name="whatsup"
+                    width="34"
+                    height="34"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div class="md:w-1/1 px-3 lg:mt-24 lg:ml-24 md:ml-12 md:mt-18 content-top">
-          <div class="text-red-600 text-3xl mb-3">
-            Hi, I'm Ilham Fatiri
+        <div
+          class="md:w-1/1 px-3 lg:ml-28 md:ml-10 content-top"
+          :class="{ 'mt-1': isMobileDevice(), 'mt-28': isDesktopDevice() }"
+        >
+          <div class="text-green-400 text-3xl mb-3">Hi, I'm Ilham Fatiri</div>
+          <div class="text-white lg:text-4xl md:text-3xl">
+            Software Engineer
           </div>
-          <div class="text-white lg:text-4xl md:text-3xl ">Software Engineer</div>
-          <div class="text-white lg:text-4xl  md:text-3xl lg:w-9/12">At Lion Parcel</div>
-          <p class="mt-2 text-gray-500 lg:text-1xl md:text-sm mt-4 lg:w-10/12 md:w-10/12">
+          <div class="text-white lg:text-4xl md:text-3xl lg:w-9/12">
+            At Lion Parcel
+          </div>
+          <p
+            class="mt-2 text-gray-500 lg:text-1xl md:text-sm mt-4 lg:w-10/12 md:w-10/12"
+          >
             &nbsp;&nbsp;&nbsp;I am a graduate of the Bootcamp Enigma Academy, I
             am educated not only to be someone who has knowledge in the field of
             software engineer, but also to become someone who has character,and
@@ -72,7 +91,10 @@
             surpass one of my teachers.
           </p>
 
-          <div class="-mx-3 flex mb-6">
+          <div
+            class="-mx-3 flex"
+            :class="{ 'mb-0': isMobileDevice(), 'mb-6': isDesktopDevice() }"
+          >
             <div
               class="md:w-1/1 custome-download-icon px-3 mb-6 md:mb-0 underline ..."
             >
@@ -90,9 +112,19 @@
 
 <script>
 import IconSvg from "../assets/svg/index.vue";
+
+import { isDesktop, isMobilePhone } from "../service/device";
 export default {
   components: {
     IconSvg,
+  },
+  methods: {
+    isMobileDevice() {
+      return isMobilePhone();
+    },
+    isDesktopDevice() {
+      return isDesktop();
+    },
   },
 };
 </script>
@@ -103,46 +135,46 @@ export default {
   height: 350px;
   filter: blur(2px);
   -webkit-filter: blur(4px);
-  background: rgba(22, 41, 38, 0.54);
-  box-shadow: 0px -2px 20px rgba(0, 0, 0, 0.25);
+  background: rgba(22, 41, 38, 0.158);
+  box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.25);
 }
 
-@media(max-width: 640px) {
+@media (max-width: 640px) {
   .profile {
-  width: 320px;
-  height: 350px;
-  margin-left: 15px;
-  filter: blur(2px);
-  -webkit-filter: blur(4px);
-  background: rgba(41, 22, 22, 0.247);
-  box-shadow: 0px -2px 20px rgba(0, 0, 0, 0.25);
-}
-.line-profile {
-  height: 4px;
-  width: 320px;
-  margin-left: 15px;
-  background-color: white;
-  margin-top: -8px;
-}
+    width: 320px;
+    height: 350px;
+    margin-left: 15px;
+    filter: blur(2px);
+    -webkit-filter: blur(4px);
+    background: rgba(41, 22, 22, 0.055);
+    box-shadow: 0px -2px 20px rgba(0, 0, 0, 0.062);
+  }
+  .line-profile {
+    height: 4px;
+    width: 320px;
+    margin-left: 15px;
+    background-color: white;
+    margin-top: -8px;
+  }
 
-.profile-image {
-  float: inline-start;
-  align-items: fixed;
-  margin-top: -375px;
-  margin-left: 50px;
-  width: 240px;
-  transform: rotate(-2deg);
-  height: 380px;
-  filter: blur(0px);
-  -webkit-filter: blur(0px);
-  animation-duration: 2s;
-  animation-iteration-count: infinite;
-  transform-origin:380px bottom;
-}
-.content-top{
-  margin-left: 20px;
- margin-top: 70px;
-}
+  .profile-image {
+    float: inline-start;
+    align-items: fixed;
+    margin-top: -375px;
+    margin-left: 50px;
+    width: 240px;
+    transform: rotate(-2deg);
+    height: 380px;
+    filter: blur(0px);
+    -webkit-filter: blur(0px);
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+    transform-origin: 380px bottom;
+  }
+  .content-top {
+    margin-left: 20px;
+    margin-top: 70px;
+  }
 }
 .profile-image {
   float: inline-start;
@@ -169,18 +201,19 @@ export default {
   50% {
     transform: translateX(-20px);
   }
-  50%{
+  50% {
     transform: rotate(-1deg);
   }
   50% {
-    transform: translateX(15px);  }
+    transform: translateX(15px);
+  }
   100% {
     transform: translateY(0);
   }
 }
 .line-profile {
   height: 4px;
-  width: 320px;
+  width: 315px;
   background-color: white;
   margin-top: -7.8px;
 }
