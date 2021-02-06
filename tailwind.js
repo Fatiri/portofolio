06ea55433d08074a3791b9a1f56c537ce95b6327
ilhamfatiri <ilham.fatiri@thelionparcel.com>
2021-02-06 08:56:14 +0700

    add custem scale on tailwind config

--- tailwind.js
+++ tailwind.js
@@ -6,11 +6,22 @@ module.exports = {
   purge: [],
   theme: {
     fontFamily: {
-      poppins:["Poppins"],
-      kanit:["Kanit"]
+      poppins: ["Poppins"],
+      kanit: ["Kanit"]
     },
-    scale:{
-      '1000':'2'
+    scale: {
+      '0': '0',
+      '25': '.25',
+      '50': '.5',
+      '75': '.75',
+      '90': '.9',
+      '95': '.95',
+      '100': '1',
+      '105': '1.05',
+      '110': '1.1',
+      '125': '1.25',
+      '150': '1.5',
+      '1000': '2'
     },
     extend: {},
   },
